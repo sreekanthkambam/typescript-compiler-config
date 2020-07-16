5990c59963535040fb5b3f126b99994501b202f5
skambam <skambam@mobileiron.com>
2020-07-16 15:37:16 +0530

    ts compiler - 1

---
+++ src/person.ts
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Sreekanth',
+    age: 30
+};
+
+console.log('Person Details - start ---------------');
+console.log('Name: ' + person.name);
+console.log('Age: ' + person.age);
+console.log('Person Details - end ---------------');
---
+++ src/properties.ts
@@ -0,0 +1,5 @@
+console.log('Property Details - start --------------');
+console.log('1. Varthur');
+console.log('2. Gattahalli');
+console.log('3. ATP');
+console.log('Property Details - end --------------');
